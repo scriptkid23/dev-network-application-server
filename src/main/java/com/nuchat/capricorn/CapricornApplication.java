09a73f0a8ab6d0b367829315e82ccd07c8478407
ICON <sieunhannhenkutesn@gmail.com>
2020-11-15 13:22:44 +0700

    first commit

---
+++ src/main/java/com/nuchat/capricorn/CapricornApplication.java
@@ -0,0 +1,13 @@
+package com.nuchat.capricorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapricornApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapricornApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nuchat/capricorn/CapricornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuchat.capricorn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapricornApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
