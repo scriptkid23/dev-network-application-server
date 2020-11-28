6cf7ec51db2ef5c07f0c27a4d41f0d018159156b
ICON <sieunhannhenkutesn@gmail.com>
2020-11-28 08:00:35 +0700

    fix bug login

--- src/main/java/com/nuchat/capricorn/dto/SigninResponseDTO.java
+++ src/main/java/com/nuchat/capricorn/dto/SigninResponseDTO.java
@@ -9,5 +9,19 @@ public class SigninResponseDTO{
         this.token = token;
     }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
--- src/main/java/com/nuchat/capricorn/model/User.java
+++ src/main/java/com/nuchat/capricorn/model/User.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Entity
 public class User{
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String first_name;
