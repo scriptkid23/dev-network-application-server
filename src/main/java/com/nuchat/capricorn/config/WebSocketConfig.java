fcdd4e59387e82888571c1eed6edd6decdade43a
ICON <sieunhannhenkutesn@gmail.com>
2020-11-30 22:08:45 +0700

    update dev

--- src/main/java/com/nuchat/capricorn/config/WebSocketConfig.java
+++ src/main/java/com/nuchat/capricorn/config/WebSocketConfig.java
@@ -4,31 +4,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-
-    private final int OUTBOUND_CHANNEL_CORE_POOL_SIZE = 8;
+    private final static String CHAT_ENDPOINT = "/ws";
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config){
-        config.enableSimpleBroker("/topic/","/queue/");
-        config.setApplicationDestinationPrefixes("/app");
-
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry.addHandler(getChatWebSocketHandler(), CHAT_ENDPOINT)
+                .setAllowedOrigins("http://localhost:4200");
     }
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:4200")
-                .withSockJS()
-                .setWebSocketEnabled(false)
-                .setSessionCookieNeeded(false);
+    @Bean
+    public WebSocketHandler getChatWebSocketHandler(){
+        return new ChatWebSocketHandler();
     }
 }
--- src/main/java/com/nuchat/capricorn/controller/GreetingController.java
+++ src/main/java/com/nuchat/capricorn/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package com.nuchat.capricorn.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
@@ -8,7 +9,8 @@ import org.springframework.stereotype.Controller;
 public class GreetingController {
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public void greeting(){
+    public void greeting(@Payload String message){
+        System.out.println(message);
 
     }
 }
