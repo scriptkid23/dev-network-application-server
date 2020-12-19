00d0ed82f93a8647f1dfde95837e242e05634a17
HoanDo <sieunhannhenkutesn@gmail.com>
2020-12-19 10:00:32 +0700

    update core function

--- src/main/java/com/nuchat/capricorn/controller/UserController.java
+++ src/main/java/com/nuchat/capricorn/controller/UserController.java
@@ -1,14 +1,37 @@
 package com.nuchat.capricorn.controller;
 
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
+
     @PutMapping("/update")
     public void updateProfile(){
-
+        //TODO:update profile of user
     }
+    @GetMapping("/{id}")
+    public void getUserInformation(@PathVariable Integer id){
+        //TODO: get information of user
+    }
+    @PostMapping("/create/room")
+    public void createRoom(){
+        //TODO: Create room by owner
+    }
+    @PostMapping("/send/request/friend")
+    public void sendRequestFriend(){
+        //TODO: Send request add friend from owner via list member
+    }
+    @DeleteMapping("unsend/request/friend")
+    public void unsendRequestFriend(){
+        //TODO: Unsend request after user send request add friend
+    }
+    @PostMapping("/send/message")
+    public void sendMessage(){
+        //TODO: send message to room
+    }
+    
+
+
+
 }
--- src/main/java/com/nuchat/capricorn/controller/WebSocketController.java
+++ src/main/java/com/nuchat/capricorn/controller/WebSocketController.java
@@ -1,5 +1,8 @@
 package com.nuchat.capricorn.controller;
 
+import com.nuchat.capricorn.service.SendEmailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -7,6 +10,8 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class WebSocketController {
+
+    private static final Logger logger = LoggerFactory.getLogger(SendEmailService.class);
     private final SimpMessagingTemplate template;
 
     WebSocketController(SimpMessagingTemplate template){
@@ -14,10 +19,35 @@ public class WebSocketController {
     }
 
     @MessageMapping("/send/message")
-    public void sendMessage(String message){
+    public void sendMessage(String message) throws  Exception{
         System.out.println(message);
         this.template.convertAndSend("/message",  message);
     }
+    // handle of status after user login into system
+    @MessageMapping("/join")
+    public void join() throws Exception{
+        logger.debug("Join");
+        template.convertAndSend(
+                "/observable/join"
+        );
+    }
+    // Observable list message log was contacted
+    @MessageMapping("/contact")
+    public void contact() throws Exception{
+        logger.debug("Contact");
+        template.convertAndSend(
+                "/observable/contact"
+        );
+    }
+    // Observable list notification between of two user
+    @MessageMapping("/notification")
+    public void notification() throws Exception{
+        logger.debug("Notification");
+        template.convertAndSend(
+                "/observable/notification"
+        );
+    }
+
 
 
 
