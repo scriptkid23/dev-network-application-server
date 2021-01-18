6c8474ae4c39811f872154f6b3137d6b06cee362
HoanDo <sieunhannhenkutesn@gmail.com>
2021-01-18 19:51:50 +0700

    update get id from list friend

--- src/main/java/com/nuchat/capricorn/sql/procedures.sql
+++ src/main/java/com/nuchat/capricorn/sql/procedures.sql
@@ -1,25 +1,29 @@
 DROP FUNCTION get_list_friend;
 CREATE OR REPLACE FUNCTION get_list_friend(userId int)
-  RETURNS TABLE (
-	  first_name varchar,
-	  last_name varchar,
-	  avatar varchar,
-	  bio varchar,
-	  birthday timestamp,
-	  email varchar,
-	  gender boolean,
-	  phone_number varchar
-  ) AS
+    RETURNS TABLE (
+                      id int,
+                      first_name varchar,
+                      last_name varchar,
+                      avatar varchar,
+                      bio varchar,
+                      birthday timestamp,
+                      email varchar,
+                      gender boolean,
+                      phone_number varchar
+                  ) AS
 $func$
 BEGIN
-   RETURN QUERY
-   SELECT user_contact.first_name, user_contact.last_name,
-   users.avatar,users.bio,users.birthday, users.email,users.gender,
-   users.phone_number
-   FROM user_contact
-   INNER JOIN users
-   ON user_contact.contact_id = users.id
-   WHERE user_id = userId;
+    RETURN QUERY
+
+        SELECT
+            users.id,
+            user_contact.first_name, user_contact.last_name,
+            users.avatar,users.bio,users.birthday, users.email,users.gender,
+            users.phone_number
+        FROM user_contact
+        INNER JOIN users
+        ON user_contact.contact_id = users.id
+        WHERE user_id = userId;
 END
 $func$  LANGUAGE plpgsql;
 
